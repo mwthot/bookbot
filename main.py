f9a29964bd7b0544285ea5a74569ecb87a48314d
mwthot <mw.thot@gmail.com>
2024-03-04 22:47:07 -0800

    Adding progress up to the character count stage.

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
     book_path = "books/frankenstein.txt" 
     book_text = get_book_text(book_path)
     num_words = word_count(book_text)
-    print(num_words)
+    num_char = char_count(book_text) 
+    print(f"This text has {num_words} words.")
+    print(f"And here is total count of each character: {num_char}")
+
+### UTILITY FUNCTIONS 
     
 def get_book_text(path): 
     with open(path) as f: 
@@ -11,5 +15,14 @@ def get_book_text(path):
 def word_count(text): 
     return len(text.split())
 
+def char_count(text):
+    char_dict = {}
+    lowered = text.lower()
+    for char in lowered:
+        if char in char_dict: 
+            char_dict[char] += 1 
+        else: 
+            char_dict[char] = 1
+    return char_dict
 
-main()
+main() 
